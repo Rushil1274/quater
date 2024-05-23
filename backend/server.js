76353f6890eff117e00f4a6903acc05c23a18dc8
Rushil1274 <118063416+Rushil1274@users.noreply.github.com>
2024-05-23 10:44:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,53 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require("mysql");
+const cors = require("cors");
 const port = 8081;
 app.use(cors());
 app.use(express.json());
+
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Gj@19122001",
-    database: "signup"
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
 
-app.post('/login', (req, res) => {
-    const sql = "SELECT * FROM login WHERE `email`=? AND `password`=?";
- 
-    db.query(sql, [req.body.email,req.body.password], (err, data) => {
-        if (err) {
-            // console.log(err);
-            return res.json("Error")
-        }
-        if (data.length>0) {
-            
-            return res.json("Success")
-        }
-        else{
-            console.log("here");
-            return res.json("Failed")
-        }
-    })
-})
+app.post("/login", (req, res) => {
+  const sql =
+    "SELECT * FROM login WHERE `email`=? AND `password`=? AND `role`=?";
+  db.query(
+    sql,
+    [req.body.email, req.body.password, req.body.role],
+    (err, data) => {
+      if (err) {
+        return res.json("Error");
+      }
+      if (data.length > 0) {
+        return res.json("Success");
+      } else {
+        return res.json("Failed");
+      }
+    }
+  );
+});
 
-app.post('/signup', (req, res) => {
-    const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES(?)";
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password,
-    ]
-    
-    db.query(sql, [values], (err, data) => {
-        if (err) {
-            return res.json("Error")
-        }
-        console.log(res.json(data));
-        return res.json(data);
-    })
-})
+app.post("/signup", (req, res) => {
+  const sql =
+    "INSERT INTO login (`name`, `email`, `password`, `role`) VALUES (?)";
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.password,
+    req.body.role,
+  ];
 
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json("Success");
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
