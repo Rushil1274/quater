156c270e07d37adf5c45213c80943adac76ca175
DeepGajjar31 <deepgajjar1238@gmail.com>
2024-05-29 14:38:49 +0530

    update

--- frontend/src/Details.js
+++ frontend/src/Details.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import img from './images/userIcon.jpg'
+import "./App.css"
 
 const Details = () => {
   return (
     <>
-      <div className='col-4'>
-        <div className='details-doc-img'>
-          <img src={img}/>
+      <div className='details-main-con'>
+
+        <div className='details-con1'>
+          <div className='details-doc-img'>
+            <img src={img}/>
+          </div>
+          <div className='details-doc-info'>
+            <p>Name: </p>
+            <p>Email: </p>
+            <p>Specialities: </p>
+            <p>Experience: </p>
+            <p>Description: </p>
+            <p>Hospital: </p>
+            <p>Address: </p>
+            <p>Fees: </p>
+          </div>
         </div>
-        <div className='details-doc-info'>
-          <p>Name: </p>
-          <p>Email: </p>
-          <p>Specialities: </p>
-          <p>Experience: </p>
-          <p>Description: </p>
-          <p>Hospital: </p>
-          <p>Address: </p>
-          <p>Fees: </p>
-        </div>
-      </div>
-      <div className='col-6'>
-        <div>
+        <div className='details-con2'>
+          <div className='details-sch-header'>
+            <h2>Doctor's Schedules</h2>
+          </div>
 
         </div>
       </div>
